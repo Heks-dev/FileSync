6d000c6e89287f961beb45b4078ba7eb927893ce
AMG <heks.dev@gmail.com>
2017-05-23 23:47:06 +0300

    Реализовано удаление файлов из destination директории которых нет в синхронизируемой source папке.

--- src/main/java/ua/com/juja/amg/filesync/SynchronizingFileVisitor.java
+++ src/main/java/ua/com/juja/amg/filesync/SynchronizingFileVisitor.java
@@ -8,7 +8,7 @@ import static java.nio.file.FileVisitResult.CONTINUE;
 
 public class SynchronizingFileVisitor extends SimpleFileVisitor<Path> {
 
-    private static final Path ROOT_PATH = Paths.get("");
+    private static final Path ROOT = Paths.get("");
     private final Path srcRoot;
     private final Path dstRoot;
     private Path currentDir;
@@ -16,7 +16,7 @@ public class SynchronizingFileVisitor extends SimpleFileVisitor<Path> {
     public SynchronizingFileVisitor(Path src, Path dest) {
         this.srcRoot = src;
         this.dstRoot = dest;
-        currentDir = ROOT_PATH;
+        currentDir = ROOT;
     }
 
     @Override
@@ -45,7 +45,7 @@ public class SynchronizingFileVisitor extends SimpleFileVisitor<Path> {
             if (hasDiffs) {
                 long sizeChanged = attrs.size() - targetFileAttrs.size();
                 Files.copy(file, targetFilePos, StandardCopyOption.REPLACE_EXISTING);
-                System.out.println(String.format("Synchronized file content: %s, size changed to (%s)", targetFilePos, sizeChanged));
+                System.out.println(String.format("Updated file: %s, size changed to (%s)", targetFilePos, sizeChanged));
             }
         }
         return CONTINUE;
@@ -53,7 +53,54 @@ public class SynchronizingFileVisitor extends SimpleFileVisitor<Path> {
 
     @Override
     public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
-        currentDir = (currentDir.getParent() != null) ? currentDir.getParent() : ROOT_PATH;
+        deleteUnnecessaryFiles(dir, dstRoot.resolve(currentDir));
+        currentDir = (currentDir.getParent() != null) ? currentDir.getParent() : ROOT;
         return CONTINUE;
+    }
+
+    private void deleteUnnecessaryFiles(Path srcCur, Path dstCur) throws IOException {
+        Files.walkFileTree(dstRoot.resolve(currentDir), new FileVisitor<Path>() {
+
+            private Path currentDir = ROOT;
+
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                boolean isRoot = dstCur.equals(dir);
+                if (!isRoot) {
+                    currentDir = currentDir.resolve(dir.getFileName());
+                }
+                return CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                Path targetFilePos = srcCur.resolve(currentDir).resolve(file.getFileName());
+                if (Files.notExists(targetFilePos)) {
+                    Files.delete(file);
+                    System.out.println("File was deleted: " + file);
+                }
+                return CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                throw exc;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                boolean isRoot = dir.getFileName().equals(dstCur.getFileName());
+                Path targetDirPos = srcCur;
+                if (!isRoot) {
+                    targetDirPos = targetDirPos.resolve(currentDir);
+                }
+                if (Files.notExists(targetDirPos)) {
+                    Files.delete(dir);
+                    System.out.println("Deleted directory: " + dir);
+                }
+                currentDir = (currentDir.getParent() != null) ? currentDir.getParent() : ROOT;
+                return CONTINUE;
+            }
+        });
     }
 }
